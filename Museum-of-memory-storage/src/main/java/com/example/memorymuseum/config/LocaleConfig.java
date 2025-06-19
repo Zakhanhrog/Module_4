e6a351ce862b9ccf7611488d679c98d812d0fde8
Ngo Gia Khanh <ngogiakhanh@MacBook-Pro-cua-Ngo.local>
2025-06-19 16:14:05 +0700

    Fix the time display function

--- Museum-of-memory-storage/src/main/java/com/example/memorymuseum/config/LocaleConfig.java
+++ Museum-of-memory-storage/src/main/java/com/example/memorymuseum/config/LocaleConfig.java
@@ -1,4 +1,4 @@
-package com.example.memorymuseum.config.i18n;
+package com.example.memorymuseum.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
